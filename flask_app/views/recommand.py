de9c4e87bc8a39edf5df2f26a30370362d55fc55
chaiwon1 <stuartandmike30@gmail.com>
2022-06-27 11:19:55 +0900

    fifififinal

--- flask_app/views/recommand.py
+++ flask_app/views/recommand.py
@@ -13,29 +13,33 @@ def recommand():
         return render_template('recommand_home.html')
 
     if request.method == 'POST' :
-        ncreds = {
+        try :
+            ncreds = {
 
                     "client_id": os.environ.get('client_id'),
                     "client_secret" : os.environ.get('client_secret')
 
                  }
-        nheaders = {
-                        "X-Naver-Client-Id" : ncreds.get('client_id'),
-                        "X-Naver-Client-Secret" : ncreds.get('client_secret')
-                    }
+            nheaders = {
+                            "X-Naver-Client-Id" : ncreds.get('client_id'),
+                            "X-Naver-Client-Secret" : ncreds.get('client_secret')
+                        }
 
-        naver_local_url = "https://openapi.naver.com/v1/search/local.json?"
+            naver_local_url = "https://openapi.naver.com/v1/search/local.json?"
 
-        location = request.form['지역']
+            location = request.form['지역']
 
-        recommands = []
-        query = location + " " + " 맛집"
-        params = "sort=comment" + "&query=" + query + "&display=" + '20'
-            
-        res = requests.get(naver_local_url + params, headers=nheaders)
-        result_list = res.json().get('items')
+            recommands = []
+            query = location + " " + " 맛집"
+            params = "sort=comment" + "&query=" + query + "&display=" + '20'
+                
+            res = requests.get(naver_local_url + params, headers=nheaders)
+            result_list = res.json().get('items')
 
-        for i in range(0,5):
-            recommands.append(result_list[i])
+            for i in range(0,5):
+                recommands.append(result_list[i])
 
-    return render_template('recommand.html', recommand=recommands, location=location)
+            return render_template('recommand.html', recommand=recommands, location=location)
+
+        except :
+            return render_template("404.html")
